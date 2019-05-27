8dca6c2b14ae910bdcf9763a0ad75fe4b2f967a8
Joe Kinney <joekinney53@gmail.com>
2019-05-26 19:19:18 -0600

    created event listener in train.js file also added moment.js

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -12,5 +12,46 @@ var firebaseConfig = {
   
   firebase.initializeApp(firebaseConfig);
 
+  var database = firebase.database();
+
+  $('#newTrain').on('click', function(){
+     
+    var trainName = $('#t-name').val().trim();
+    var destination = $('#t-destination').val().trim();
+    var firstTrain = $('#firstT-time').val().trim();
+    var frequency = $('#t-frequency').val().trim();
+
+    var newTrain = {
+      name: trainName,
+      dest: destination,
+      first: firstTrain,
+      freq: frequency,
+
+    };
+
+    database.ref().push(newTrain);
+
+    console.log(newTrain.name);
+    console.log(newTrain.dest);
+    console.log(newTrain.first);
+    console.log(newTrain.freq);
+
+    $('#t-name').val("");
+    $('#t-destination').val("");
+    $('#firstT-time').val("");
+    $('#t-frequency').val("");
+
+
+    return false;
+
+
+
+  })
+  
+  
+
+
+
+
 
 
