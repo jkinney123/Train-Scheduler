b7743a4801414805ded0e256a8b9727ed3b45fab
Joe Kinney <joekinney53@gmail.com>
2019-05-26 18:18:13 -0600

    created html table and added firebase ajax in train.js file

---
+++ assets/javascript/train.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+var firebaseConfig = {
+    apiKey: "AIzaSyB1sUmcNiYLYnynUiyO9XMebrBcLUmTLYM",
+    authDomain: "fire-base-3afae.firebaseapp.com",
+    databaseURL: "https://fire-base-3afae.firebaseio.com",
+    projectId: "fire-base-3afae",
+    storageBucket: "fire-base-3afae.appspot.com",
+    messagingSenderId: "491171645798",
+    appId: "1:491171645798:web:d829903de544d45f"
+  };
+  
+  firebase.initializeApp(firebaseConfig);
+
+
+
